877a961988b0da0636166a77a9cf09531abd581d
Max D. <35872787+maxDavid40@users.noreply.github.com>
2025-04-17 09:21:37 +0200

    add launch arguments endpoints (#3)
    * add launch arguments endpoints
    
    * fix error
    
    * remove argument from OpenAIServing class
    
    * Changes from review
    
    * Add launch_arguments tests
    
    ---------
    
    Co-authored-by: Gautier Solard <gautier.solard@francetravail.fr>

--- vllm/entrypoints/openai/cli_args.py
+++ vllm/entrypoints/openai/cli_args.py
@@ -304,6 +304,11 @@ def make_arg_parser(parser: FlexibleArgumentParser) -> FlexibleArgumentParser:
         "Must be a YAML with the following options: "
         "https://docs.vllm.ai/en/latest/configuration/serve_args.html",
     )
+    parser.add_argument("--enable-launch-arguments",
+                        action="store_true",
+                        default=False,
+                        help="Whether the route launch_arguments should display the launch arguments")
+    
     parser = FrontendArgs.add_cli_args(parser)
     parser = AsyncEngineArgs.add_cli_args(parser)
 
