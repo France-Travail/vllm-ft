74f73145d4bb89bba7fae07d86e408d91fa445b3
Gautier Solard <104368859+gsolard@users.noreply.github.com>
2025-04-29 17:04:05 +0200

    Add /v1/info route (#5)
    * Add /v1/info route
    
    * added test

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -57,6 +57,7 @@ from vllm.utils.argparse_utils import FlexibleArgumentParser
 from vllm.utils.network_utils import is_valid_ipv6_address
 from vllm.utils.system_utils import decorate_logs, set_ulimit
 from vllm.version import __version__ as VLLM_VERSION
+from vllm.version import ORIGINAL_VLLM_VERSION
 
 prometheus_multiproc_dir: tempfile.TemporaryDirectory
 
@@ -295,6 +296,7 @@ async def init_app_state(
         served_model_names = args.served_model_name
     else:
         served_model_names = [args.model]
+    state.served_model_names = served_model_names
 
     if args.enable_launch_arguments:
         clean_arguments = {key: value.__name__ if isinstance(value, Callable) else value
@@ -370,6 +372,7 @@ async def init_app_state(
 
     state.enable_server_load_tracking = args.enable_server_load_tracking
     state.server_load_metrics = 0
+    state.model_config = model_config
 
 
 def create_server_socket(addr: tuple[str, int]) -> socket.socket:
