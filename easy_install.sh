f954d63d309be48d1b8ac78d8533aaac0051dc14
Gautier Solard <104368859+gsolard@users.noreply.github.com>
2025-05-02 15:10:59 +0200

    Install vLLM with explicit precompiled wheel (#9)
    * Install vLLM with explicit precompiled wheel
    
    * Delete useless line

--- easy_install.sh
+++ easy_install.sh
@@ -4,4 +4,6 @@ cd requirements
 pip install -r common.txt
 pip install -r build.txt
 cd ..
-VLLM_USE_PRECOMPILED=1 pip install --editable .
+export VLLM_COMMIT=ed2462030f2ccc84be13d8bb2c7476c84930fb71 # use full commit hash from the main branch
+export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl
+pip install --editable .
