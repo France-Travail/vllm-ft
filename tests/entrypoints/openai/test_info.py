b07007afecaf7f3e83b5e2f821d0e6d90ff1e4b0
Gautier Solard <104368859+gsolard@users.noreply.github.com>
2025-05-06 16:30:54 +0200

    Add extra information (#14)
    * Added extra_information
    
    * add import json
    
    * Added default value and tests

--- tests/entrypoints/openai/test_info.py
+++ tests/entrypoints/openai/test_info.py
@@ -1,3 +1,5 @@
+import os
+import json
 import pytest
 import pytest_asyncio
 import requests
@@ -7,6 +9,7 @@ from typing import Callable
 from ...utils import RemoteOpenAIServer
 from vllm.entrypoints.openai import utils_ft
 from vllm.version import ORIGINAL_VLLM_VERSION
+from ...conftest import _TEST_DIR
 
 # any model should work
 MODEL_NAME = "facebook/opt-125m"
@@ -41,11 +44,29 @@ def server(
     with RemoteOpenAIServer(MODEL_NAME, args) as remote_server:
         yield remote_server
 
+@pytest.fixture(scope="function", params=[False, True])
+def server_with_extra_information(
+        request,
+        monkeypatch_module):
 
-@pytest_asyncio.fixture
-async def client(server):
-    async with server.get_async_client() as async_client:
-        yield async_client
+    use_v1 = request.param
+    monkeypatch_module.setenv('VLLM_USE_V1', '1' if use_v1 else '0')
+    extra_information_path = str(os.path.join(_TEST_DIR, "entrypoints", "openai", "data", "extra_information.json"))
+    args = [
+        # use half precision for speed and memory savings in CI environment
+        "--dtype",
+        "bfloat16",
+        "--max-model-len",
+        "333",
+        "--enforce-eager",
+        "--max-num-seqs",
+        "13",
+        "--extra-information",
+        f"{extra_information_path}"
+    ]
+
+    with RemoteOpenAIServer(MODEL_NAME, args) as remote_server:
+        yield remote_server
 
 
 @pytest.mark.asyncio
@@ -60,6 +81,31 @@ async def test_info_with_curl(server: RemoteOpenAIServer):
                        "application": "vllm_ft",
                        "version": utils_ft.get_package_version(),
                        "vllm_version": ORIGINAL_VLLM_VERSION,
-                       "max_length": 333}
+                       "max_length": 333,
+                       "extra_information": {}}
     for key, value in target_response.items():
         assert response[key] == value
+
+
+@pytest.mark.asyncio
+async def test_info_with_extra_information(server_with_extra_information: RemoteOpenAIServer):
+    url = f"http://localhost:{server_with_extra_information.port}/v1/info"
+    headers = {
+        "Content-Type": "application/json",
+    }
+    extra_information_path = str(os.path.join(_TEST_DIR, "entrypoints", "openai", "data", "extra_information.json"))
+    with open(extra_information_path, 'r') as json_file:
+        extra_information = json.load(json_file)
+    response = requests.get(url, headers=headers).json()
+    target_response = {"model_name": MODEL_NAME,
+                       "application": "vllm_ft",
+                       "version": utils_ft.get_package_version(),
+                       "vllm_version": ORIGINAL_VLLM_VERSION,
+                       "max_length": 333,
+                       "extra_information": extra_information}
+    for key, value in target_response.items():
+        if key == "extra_information":
+            for key_extra_information, value_extra_information in value.items():
+                assert response["extra_information"][key_extra_information] == value_extra_information
+        else:
+            assert response[key] == value
