ec0f6cb21d2347d95827e4d76ee942162d53564e
Gautier Solard <104368859+gsolard@users.noreply.github.com>
2025-05-09 08:52:56 +0200

    Updated dockerfile (#16)
    Add the action to publish to pypi (#17)
    
    Added readiness and liveness routes (#18)
    
    Update publish action (#19)
    
    Fix a typo in publish action (#20)
    
    Properly set the environment publish (#21)
    
    Directly put VLLM_PRECOMPILED_WHEEL_LOCATION in variable (#22)
    
    Update the publish action inspired by vLLM (#23)
    
    Move scripts (#24)
    
    Add pytorch-install script (#25)
    
    Delete cuda in torch (#26)
    
    Update library name (#27)
    
    Add verbose (#28)
    
    Checkout to the latest tag in publish action (#29)
    
    Fix typo -- publish action
    
    Fix typo -- publish action (#30)
    
    Delete the precompiled in the version (#31)
    
    Use cibuildwheel in publish action (#32)
    
    Cleanup the repo

--- easy_install.sh
+++ easy_install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-cd requirements
-pip install -r common.txt
-pip install -r build.txt
-cd ..
 export VLLM_COMMIT=ed2462030f2ccc84be13d8bb2c7476c84930fb71 # use full commit hash from the main branch
 export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl
-pip install --editable .
+if [ "$1" == "--env-only" ]; then
+    echo $VLLM_PRECOMPILED_WHEEL_LOCATION
+else
+    {
+        cd requirements
+        pip install -r common.txt
+        pip install -r build.txt
+        cd ..
+        pip install --editable .
+    }
+fi
