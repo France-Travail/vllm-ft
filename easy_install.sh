91e4ba8eccc669bb5f113c23a1d0f09357372175
Gautier Solard <104368859+gsolard@users.noreply.github.com>
2025-05-09 16:07:06 +0200

    Directly put VLLM_PRECOMPILED_WHEEL_LOCATION in variable (#22)

--- easy_install.sh
+++ easy_install.sh
@@ -3,7 +3,7 @@
 export VLLM_COMMIT=ed2462030f2ccc84be13d8bb2c7476c84930fb71 # use full commit hash from the main branch
 export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl
 if [ "$1" == "--env-only" ]; then
-    echo $VLLM_COMMIT
+    echo $VLLM_PRECOMPILED_WHEEL_LOCATION
 else
     {
         cd requirements
