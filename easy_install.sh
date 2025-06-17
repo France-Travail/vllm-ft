de56a90de2e711f11b707b029093380837c1cb81
maxDavid40 <maxdavid40@gmail.com>
2025-06-17 17:15:28 +0200

    fix typo

--- easy_install.sh
+++ easy_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VLLM_TAG=0.9.1 # use full commit hash from the main branch
+export VLLM_TAG="" # use current tag version vllm
 export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_TAG}/vllm-${VLLM_TAG}-cp38-abi3-manylinux1_x86_64.whl
 if [ "$1" == "--env-only" ]; then
     echo $VLLM_PRECOMPILED_WHEEL_LOCATION
