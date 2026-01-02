e529da5c56e6b82d6658e567233d015b34827936
maxDavid40 <maxdavid40@gmail.com>
2026-01-02 15:40:21 +0100

    Fix change api server + url wheel

--- easy_install.sh
+++ easy_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-export VLLM_TAG=0.11.0 # use current tag version vllm
-export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_TAG}/vllm-${VLLM_TAG}-cp38-abi3-manylinux1_x86_64.whl
+export VLLM_TAG=0.13.0 # use current tag version vllm
+export VLLM_COMMIT=72506c98349d6bcd32b4e33eec7b5513453c1502
+export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_COMMIT}/vllm-${VLLM_TAG}-cp38-abi3-manylinux_2_31_x86_64.whl
 if [ "$1" == "--env-only" ]; then
     echo $VLLM_PRECOMPILED_WHEEL_LOCATION
 else
