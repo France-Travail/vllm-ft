a642cec2d7bf0ac76de56a6cd0123955412bd06d
Max D. <35872787+maxDavid40@users.noreply.github.com>
2026-02-03 15:08:38 +0100

    move /v1/info and /v1/launch_arguments (#43)

--- vllm/entrypoints/openai/models/api_router.py
+++ vllm/entrypoints/openai/models/api_router.py
@@ -27,3 +27,32 @@ async def show_available_models(raw_request: Request):
 
 def attach_router(app: FastAPI):
     app.include_router(router)
+
+
+from vllm.entrypoints.openai import utils_ft
+from vllm.version import ORIGINAL_VLLM_VERSION
+
+
+@router.get("/v1/launch_arguments")
+async def show_launch_arguments(raw_request: Request):
+    if raw_request.app.state.arguments is None:
+        return base(raw_request).create_error_response(
+            message="Launch arguments is not enabled")
+    else:
+        return JSONResponse(content=raw_request.app.state.arguments)
+
+
+@router.get("/v1/info")
+async def get_info(raw_request: Request):
+    model_name = None
+    if raw_request.app.state.served_model_names:
+        model_name = raw_request.app.state.served_model_names[0]
+    content = {
+        "application": "vllm_ft",
+        "version": utils_ft.get_package_version().split("+")[0],
+        "vllm_version": ORIGINAL_VLLM_VERSION,
+        "model_name": model_name,
+        "max_length": raw_request.app.state.model_config.max_model_len,
+        "extra_information": raw_request.app.state.extra_information
+    }
+    return JSONResponse(content=content)
