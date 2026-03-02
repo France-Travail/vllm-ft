4ea4a4ff7bf34984c53f277e80f518fdcc6311a6
maxDavid40 <maxdavid40@gmail.com>
2026-03-02 15:17:11 +0100

    0.16.0 vllm version

--- easy_install.sh
+++ easy_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export VLLM_TAG=0.13.0 # use current tag version vllm
-export VLLM_COMMIT=72506c98349d6bcd32b4e33eec7b5513453c1502
+export VLLM_TAG=0.16.0 # use current tag version vllm
+export VLLM_COMMIT=89a77b10846fd96273cce78d86d2556ea582d26e
 export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_COMMIT}/vllm-${VLLM_TAG}-cp38-abi3-manylinux_2_31_x86_64.whl
 if [ "$1" == "--env-only" ]; then
     echo $VLLM_PRECOMPILED_WHEEL_LOCATION
