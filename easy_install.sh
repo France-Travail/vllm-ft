a559fbea7f746f7b30ed2ce4a4ab82eadbd58ee8
Max D. <35872787+maxDavid40@users.noreply.github.com>
2026-03-17 08:34:49 +0100

    remove setuptools cmd (#46)

--- easy_install.sh
+++ easy_install.sh
@@ -25,7 +25,6 @@ fi
 if [ "$1" == "--env-only" ]; then
     echo $VLLM_PRECOMPILED_WHEEL_LOCATION
 else
-    export SETUPTOOLS_SCM_PRETEND_VERSION=${VLLM_TAG}
     {
         cd requirements
         pip install -r common.txt
