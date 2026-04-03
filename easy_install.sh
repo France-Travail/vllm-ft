9fd1800e513087a607d6d2c48ddd1956d573ff8e
maxDavid40 <maxdavid40@gmail.com>
2026-04-03 11:26:15 +0200

    update version + easy_install.sh

--- easy_install.sh
+++ easy_install.sh
@@ -3,25 +3,20 @@ export VLLM_TAG=$(grep "ORIGINAL_VLLM_VERSION" vllm/version.py | cut -d '"' -f 2
 
 echo "vLLM's version : ${VLLM_TAG}"
 
+
 WHEEL_NAME="vllm-${VLLM_TAG}-cp38-abi3-manylinux_2_31_x86_64.whl"
-echo "Searching for the wheel's commit ${WHEEL_NAME}..."
-VLLM_COMMIT=$(curl -sL "https://wheels.vllm.ai/${VLLM_TAG}/vllm" | grep -oE "[a-f0-9]{40}/${WHEEL_NAME}" | head -n 1 | cut -d '/' -f 1)
-if [ -z "$VLLM_COMMIT" ]; then
-    echo "Error: Could not find a matching commit for wheel ${WHEEL_NAME} on the release page."
-    exit 1
-fi
-export VLLM_COMMIT
-echo "Commit found : ${VLLM_COMMIT}"
-export VLLM_PRECOMPILED_WHEEL_LOCATION="https://wheels.vllm.ai/${VLLM_COMMIT}/${WHEEL_NAME}"
+
+export VLLM_PRECOMPILED_WHEEL_LOCATION="https://github.com/vllm-project/vllm/releases/download/v${VLLM_TAG}/${WHEEL_NAME}"
 
 # Check if URL is OK
 echo "Wheel's url : ${VLLM_PRECOMPILED_WHEEL_LOCATION}"
-HTTP_STATUS=$(curl -o /dev/null -s -I -w "%{http_code}\n" "$VLLM_PRECOMPILED_WHEEL_LOCATION")
+HTTP_STATUS=$(curl -L -o -v /dev/null -s -I -w "%{http_code}\n" "$VLLM_PRECOMPILED_WHEEL_LOCATION")
 if [ "$HTTP_STATUS" -ne 200 ]; then
     echo "Error: wheel's url not recheable (HTTP's error $HTTP_STATUS)"
     exit 1
 fi
 
+exit 1
 if [ "$1" == "--env-only" ]; then
     echo $VLLM_PRECOMPILED_WHEEL_LOCATION
 else
