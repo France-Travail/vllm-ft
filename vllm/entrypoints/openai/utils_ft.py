95160c81594d98fee54acdbd9a2b171c32291959
Max D. <35872787+maxDavid40@users.noreply.github.com>
2026-04-09 10:32:27 +0200

    fix vllm ft version import (#50)
    * fix vllm ft version import
    
    * Remove try

--- vllm/entrypoints/openai/utils_ft.py
+++ vllm/entrypoints/openai/utils_ft.py
@@ -1,9 +1,9 @@
-import importlib.metadata
+from vllm.ft_version import FORK_VERSION
+
 
 def get_package_version() -> str:
     '''Returns the current version of the package
     Returns:
         str: version of the package
     '''
-    version = importlib.metadata.version("vllm")
-    return version
+    return FORK_VERSION.replace("v", "").split("+")[0]
