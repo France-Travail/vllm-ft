43850e73396590c4eb7f21f09742d78c643978fc
maxDavid40 <maxdavid40@gmail.com>
2026-04-09 10:11:58 +0200

    fix vllm ft version import

--- easy_install.sh
+++ easy_install.sh
@@ -32,6 +32,10 @@ if [ "$1" == "--env-only" ]; then
     echo $VLLM_PRECOMPILED_WHEEL_LOCATION
 else
     {
+        export SETUPTOOLS_SCM_PRETEND_VERSION="${VLLM_TAG}"
+        export VLLM_VERSION="${VLLM_TAG}"
+        FORK_TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+        echo "FORK_VERSION = \"${FORK_TAG}\"" > vllm/ft_version.py
         cd requirements
         echo "Installation des dépendances depuis ${REQ_FILE}..."
         pip install -r "$REQ_FILE"
--- vllm/entrypoints/openai/utils_ft.py
+++ vllm/entrypoints/openai/utils_ft.py
@@ -1,9 +1,14 @@
 import importlib.metadata
 
+try:
+    from vllm.ft_version import FORK_VERSION
+except ImportError:
+    FORK_VERSION = "unknown"
+
+
 def get_package_version() -> str:
     '''Returns the current version of the package
     Returns:
         str: version of the package
     '''
-    version = importlib.metadata.version("vllm")
-    return version
+    return FORK_VERSION.replace("v", "").split("+")[0]
