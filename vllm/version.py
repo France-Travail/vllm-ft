615532fbc61c0dc8a02d04ae1c96b375303c9eda
Drikain <cedric.duval.44@gmail.com>
2026-04-21 10:19:11 +0200

    version update

--- vllm/version.py
+++ vllm/version.py
@@ -12,7 +12,7 @@ except Exception as e:
     __version_tuple__ = (0, 0, __version__)
 
 
-ORIGINAL_VLLM_VERSION = "0.16.0"
+ORIGINAL_VLLM_VERSION = "0.19.1"
 
 
 def _prev_minor_version_was(version_str):
